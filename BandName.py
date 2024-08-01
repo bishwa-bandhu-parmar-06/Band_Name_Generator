af5f997409a93117b7557393796a99588cb48a5e
BISHWA BANDHU PARMAR <bishwabandhuparmar06@gmail.com>
2024-08-01 20:18:22 +0530

    Add files via upload

---
+++ BandName.py
@@ -0,0 +1,11 @@
+# Creating a greeting for your program.
+print("Welcome to The Band Name Generator.")
+
+# Ask the user for the city where they grew up.
+city = input("What is Your Name of City Where you Grew Up?\n")
+
+# Ask the user for the name of pet.
+petName = input("What Is your Pet Name?\n")
+
+# combine the name of city and pet name and show their band name
+print("Your Band Name could be : " + city +" " + petName)
